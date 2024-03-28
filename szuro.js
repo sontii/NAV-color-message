80d7d79f3d42e3619844775dea8fc27163157d62
Fridrich Ferenc <sontii78@gmail.com>
2024-03-28 20:09:41 +0100

    .

--- szuro.js
+++ szuro.js
@@ -1,24 +1,70 @@
 function changeBGColors() {
-	$("td:contains('FeladasiIgazolas')").css( "color", "#ddd" );
-	$("td:contains('LetoltesiIgazolas')").css( "color", "#ddd" );
-	$("td:contains('NAV')").css( "backgroundColor", "yellow" );
-	$("td:contains('Hivatal')").css( "backgroundColor", "yellow" );
-	$("td:contains('CEGINFO')").css( "backgroundColor", "yellow" );
-	$("td:contains('Értesítés - Tájékoztatás')").css( "backgroundColor", "rgb(227, 236, 255)" );
-	$("td:contains('Elektronikus értesítés')").css( "backgroundColor", "rgb(227, 236, 255)" );
-	$("td:contains('IGAZOL, Igazolás')").css( "backgroundColor", "rgb(227, 236, 255)" );
-	$("td:contains('OEP_foglalkoztatoi_ig_V')").css( "backgroundColor", "rgb(227, 236, 255)" );
-	$("td:contains('Elfogadott bizonylat')").css( "backgroundColor", "rgb(188, 255, 187)" );
-	$("td:contains('Értesítés - Intézkedés')").css( "backgroundColor", "rgb(255, 198, 198)" );
-	$("td:contains('Elutasító nyugta')").css( "backgroundColor", "rgb(255, 198, 198)" );
-	$("td:contains('Felszólítás')").css( "backgroundColor", "rgb(255, 198, 198)" );
-	$("td:contains('Elutasítás')").css( "backgroundColor", "rgb(255, 198, 198)" );
-	$("td:contains('hibaüzenet')").css( "backgroundColor", "rgb(255, 198, 198)" );
+	var trs = document.querySelectorAll("tr")
+
+	trs.forEach(function (tr) {
+		var tds = tr.querySelectorAll("td")
+
+		// Iterate over the <td> elements
+		tds.forEach(function (td, index) {
+			var prevPrevTD = index > 0 ? tds[index - 2] : null // Select previous previous <td> if it exists
+			var prevTD = index > 0 ? tds[index - 1] : null // Select previous <td> if it exists
+			var secondTD = td // Current <td>
+			var nextTD = index + 1 < tds.length ? tds[index + 1] : null // Select next <td> if it exists
+
+			if (index === 2 && prevPrevTD &&prevTD && secondTD && nextTD) {
+				// Check the content of the second <td> element only
+				if (
+					secondTD.textContent.includes("FeladasiIgazolas") ||
+					secondTD.textContent.includes("LetoltesiIgazolas")
+				) {
+					prevPrevTD.style.backgroundColor = "#ddd"
+					prevTD.style.backgroundColor = "#ddd"
+					secondTD.style.backgroundColor = "#ddd"
+					nextTD.style.backgroundColor = "#ddd"
+				} else if (
+					secondTD.textContent.includes("NAV") ||
+					secondTD.textContent.includes("Hivatal") ||
+					secondTD.textContent.includes("CEGINFO")
+				) {
+					prevPrevTD.style.backgroundColor = "#FFFF00"
+					prevTD.style.backgroundColor = "#FFFF00"
+					secondTD.style.backgroundColor = "#FFFF00"
+					nextTD.style.backgroundColor = "#FFFF00"
+				} else if (
+					secondTD.textContent.includes("Értesítés - Tájékoztatás") ||
+					secondTD.textContent.includes("Elektronikus értesítés") ||
+					secondTD.textContent.includes("IGAZOL, Igazolás") ||
+					secondTD.textContent.includes("OEP_foglalkoztatoi_ig_V")
+				) {
+					prevPrevTD.style.backgroundColor = "#E3ECFF"
+					prevTD.style.backgroundColor = "#E3ECFF"
+					secondTD.style.backgroundColor = "#E3ECFF"
+					nextTD.style.backgroundColor = "#E3ECFF"
+				} else if (secondTD.textContent.includes("Elfogadott bizonylat")) {
+					prevPrevTD.style.backgroundColor = "#BCFFBB"
+					prevTD.style.backgroundColor = "#BCFFBB"
+					secondTD.style.backgroundColor = "#BCFFBB"
+					nextTD.style.backgroundColor = "#BCFFBB"
+				} else if (
+					secondTD.textContent.includes("Értesítés - Intézkedés") ||
+					secondTD.textContent.includes("Elutasító nyugta") ||
+					secondTD.textContent.includes("Felszólítás") ||
+					secondTD.textContent.includes("Elutasítás") ||
+					secondTD.textContent.includes("hibaüzenet")
+				) {
+					prevPrevTD.style.backgroundColor = "#FFCCCC"
+					prevTD.style.backgroundColor = "#FFCCCC"
+					secondTD.style.backgroundColor = "#FFCCCC"
+					nextTD.style.backgroundColor = "#FFCCCC"
+				}
+			}
+		})
+	})
 }
 
-var target = document.body;
-var observer = new MutationObserver(function(mutation){
-	changeBGColors();
-});
-var config = {childList: true, subtree:true};
-observer.observe(target,config);
+var target = document.body
+var observer = new MutationObserver(function (mutation) {
+	changeBGColors()
+})
+var config = { childList: true, subtree: true }
+observer.observe(target, config)
