0f489c9df1f89f5ecdf97c0e71d1004de3d4f1eb
sontii <sontii78@gmail.com>
2024-03-28 13:08:25 +0100

    .

--- szuro.js
+++ szuro.js
@@ -1,19 +1,19 @@
 function changeBGColors() {
-	$("td:conntains('FeladasiIgazolas')").css( "color", "#ddd" );
-	$("td:conntains('LetoltesiIgazolas')").css( "color", "#ddd" );
-	$("td:conntains('NAV')").css( "backgroundColor", "yellow" );
-	$("td:conntains('Hivatal')").css( "backgroundColor", "yellow" );
-	$("td:conntains('CEGINFO')").css( "backgroundColor", "yellow" );
-	$("td:conntains('Értesítés - Tájékoztatás')").css( "backgroundColor", "rgb(227, 236, 255)" );
-	$("td:conntains('Elektronikus értesítés')").css( "backgroundColor", "rgb(227, 236, 255)" );
-	$("td:conntains('IGAZOL, Igazolás')").css( "backgroundColor", "rgb(227, 236, 255)" );
-	$("td:conntains('OEP_foglalkoztatoi_ig_V')").css( "backgroundColor", "rgb(227, 236, 255)" );
-	$("td:conntains('Elfogadott bizonylat')").css( "backgroundColor", "rgb(188, 255, 187)" );
-	$("td:conntains('Értesítés - Intézkedés')").css( "backgroundColor", "rgb(255, 198, 198)" );
-	$("td:conntains('Elutasító nyugta')").css( "backgroundColor", "rgb(255, 198, 198)" );
-	$("td:conntains('Felszólítás')").css( "backgroundColor", "rgb(255, 198, 198)" );
-	$("td:conntains('Elutasítás')").css( "backgroundColor", "rgb(255, 198, 198)" );
-	$("td:conntains('hibaüzenet')").css( "backgroundColor", "rgb(255, 198, 198)" );
+	$("tr:contains('FeladasiIgazolas')").css( "color", "#ddd" );
+	$("tr:contains('LetoltesiIgazolas')").css( "color", "#ddd" );
+	$("tr:contains('NAV')").css( "backgroundColor", "yellow" );
+	$("tr:contains('Hivatal')").css( "backgroundColor", "yellow" );
+	$("tr:contains('CEGINFO')").css( "backgroundColor", "yellow" );
+	$("tr:contains('Értesítés - Tájékoztatás')").css( "backgroundColor", "rgb(227, 236, 255)" );
+	$("tr:contains('Elektronikus értesítés')").css( "backgroundColor", "rgb(227, 236, 255)" );
+	$("tr:contains('IGAZOL, Igazolás')").css( "backgroundColor", "rgb(227, 236, 255)" );
+	$("tr:contains('OEP_foglalkoztatoi_ig_V')").css( "backgroundColor", "rgb(227, 236, 255)" );
+	$("tr:contains('Elfogadott bizonylat')").css( "backgroundColor", "rgb(188, 255, 187)" );
+	$("tr:contains('Értesítés - Intézkedés')").css( "backgroundColor", "rgb(255, 198, 198)" );
+	$("tr:contains('Elutasító nyugta')").css( "backgroundColor", "rgb(255, 198, 198)" );
+	$("tr:contains('Felszólítás')").css( "backgroundColor", "rgb(255, 198, 198)" );
+	$("tr:contains('Elutasítás')").css( "backgroundColor", "rgb(255, 198, 198)" );
+	$("tr:contains('hibaüzenet')").css( "backgroundColor", "rgb(255, 198, 198)" );
 }
 
 var target = document.body;
